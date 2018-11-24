74ed3ce2715959443ba9f503b71d51753c00171d
Surat Teerapittayanon <steerapi@gmail.com>
2018-11-24 01:19:37 -0500

    using standard format

--- src/sig-server/index.js
+++ src/sig-server/index.js
@@ -1,41 +1,42 @@
-'use strict';
+'use strict'
 
-const Hapi = require('hapi');
-const config = require('./config');
-const log = config.log;
-const epimetheus = require('epimetheus');
-const path = require('path');
+const Hapi = require('hapi')
+const config = require('./config')
+const log = config.log
+const epimetheus = require('epimetheus')
+const path = require('path')
 
-exports = module.exports;
+exports = module.exports
 
 exports.start = async (options = {}) => {
-  const port = options.port || config.hapi.port;
-  const host = options.host || config.hapi.host;
+  const port = options.port || config.hapi.port
+  const host = options.host || config.hapi.host
 
   const http = new Hapi.Server({
     port: port,
     host: host
-  });
+  })
 
-  await http.register({ plugin: require('inert') });
-  await http.start();
+  await http.register({ plugin: require('inert') })
+  await http.start()
 
-  log('signaling server has started on: ' + http.info.uri);
+  log('signaling server has started on: ' + http.info.uri)
 
-  http.peers = require('./routes-ws')(http, options.metrics).peers;
+  http.peers = require('./routes-ws')(http, options.metrics).peers
 
   http.route({
     method: 'GET',
     path: '/',
-    handler: (request, reply) =>
+    handler: (request, reply) => {
       reply.file(path.join(__dirname, 'index.html'), {
         confine: false
       })
-  });
+    }
+  })
 
   if (options.metrics) {
-    epimetheus.instrument(http);
+    epimetheus.instrument(http)
   }
 
-  return http;
-};
+  return http
+}
